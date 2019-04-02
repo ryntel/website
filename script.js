5c2dc161f98eec3d25a545acdeda6fb8d083622d
hisakiyo <34187647+hisakiyo@users.noreply.github.com>
2019-04-02 22:05:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ function toggleDarkLight() {
     var body = document.getElementById("body");
     var currentClass = body.className;
     body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+	if(currentClass == 'light-mode'){	
+	document.getElementById("a_changer").classList.add('text-white');
+	document.getElementById("a_changer").classList.add('bg-dark');
+	} else {
+	document.getElementById("a_changer").classList.remove('text-white');
+	document.getElementById("a_changer").classList.remove('bg-dark');
   }
+}
